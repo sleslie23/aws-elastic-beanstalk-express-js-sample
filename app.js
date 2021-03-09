fb44a2ce9464843fbf6533f4627f994857ef4a76
Steven Leslie <sleslie23@icloud.com>
2021-03-08 22:56:04 -0800

    Trying to fail

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Pipeline should be automated!'));
+app.get('/', (req, res) => res.send('Another test!'));blakjdsf
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
